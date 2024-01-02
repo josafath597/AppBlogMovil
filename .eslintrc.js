af0789cd31b0869147b9312cf8dfde0a065a6714
Fabian Cardenas <fcardenasaldana@gmail.com>
2024-01-02 14:30:54 -0600

    Se inicializa el Repositorio

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: 'airbnb',
+  overrides: [
+    {
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/react-in-jsx-scope': 'off',
+    'no-use-before-define': ['error', { variables: false }],
+  },
+};
